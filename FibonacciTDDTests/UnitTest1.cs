05f35ac55bb441d95e5d3848fef4e9fea9614aa8
Mamadou Bah <m.bah1705@gmail.com>
2018-09-13 20:46:24 +0200

    Created FibonacciTDDTests project and referenced the FibonacciTDD project

---
+++ FibonacciTDDTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FibonacciTDDTests
+{
+    [TestClass]
+    public class UnitTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
