6a9147b5085ff25e9bbd84edb92606cb2193e39a
Mamadou Bah <m.bah1705@gmail.com>
2018-09-13 22:13:29 +0200

    Added FibonacciController and FibonacciCalculator class, then wrote 2 obvious unit tests that passed

---
+++ FibonacciTDD/Logic/FibonacciCalculator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FibonacciTDD.Logic
+{
+    public class FibonacciCalculator
+    {
+        public int CalculateValue(int v)
+        {
+            return v;
+        }
+    }
+}
---
+++ FibonacciTDDTests/UnitTests.cs
@@ -0,0 +1,23 @@
+using FibonacciTDD.Logic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FibonacciTDDTests
+{
+    [TestClass]
+    public class UnitTests
+    {
+        [TestMethod]
+        public void Fibonacci_When1_ShouldReturn1()
+        {
+            FibonacciCalculator Calculator = new FibonacciCalculator();
+            Assert.AreEqual(1, Calculator.CalculateValue(1));
+        }
+
+        [TestMethod]
+        public void Fibonacci_When2_ShouldReturn2()
+        {
+            FibonacciCalculator Calculator = new FibonacciCalculator();
+            Assert.AreEqual(2, Calculator.CalculateValue(2));
+        }
+    }
+}
